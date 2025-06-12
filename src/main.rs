7e25a1b92556f393496a7b9f3b173f7f560f439a
Brahmantara Putra W <BrahmantaraPutraW@gmail.com>
2025-06-12 21:53:29 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -123,3 +123,199 @@ fn boolean (){
     let b : bool = false;
     print!("{} {}", a, b);
 }
+
+
+#[test]
+fn comper (){
+    let a = 10;
+    let b = 20;
+
+    let result : bool  = a > b;
+    print!("{}", result);
+
+    /*let result : bool  = a < b;
+    print!("{}", result);
+
+    let result : bool  = a >= b;
+    print!("{}", result);
+
+    let result : bool  = a <= b;
+    print!("{}", result);
+
+    let result : bool  = a == b;
+    print!("{}", result);*/
+}
+
+
+//pake (&&, ||, !)
+#[test]
+fn operatorbl (){
+    let absen = 75;
+    let nilai = 800;
+
+    let lulus  = absen >= 70;
+    let lulus_nilai_akhir = nilai >= 70;
+
+    let lulus_final= lulus && lulus_nilai_akhir;
+    print!("{}", lulus_final);
+}
+
+#[test]
+fn char_type(){
+    let char1 : char = 'a';
+    let char2 : char = 'b';
+
+    print!("{} {}", char1, char2);
+}
+
+//tuple
+#[test]
+fn tuple(){
+    let data : (i32,f64,bool) = (10,7.7,true);
+    print!("{:?}", data);
+
+    let a = data.0;
+    let b = data.1;
+    let c = data.2;
+    print!("{} {} {}", a, b, c);
+}
+
+//tuple
+#[test]
+fn dec_tuple(){
+    let data : (i32,f64,bool) = (10,7.7,true);
+    print!("{:?}", data);
+
+    let (_, b, c) = data; 
+    print!("{} {}",b, c);
+}
+
+//mutable tuple
+#[test]
+fn tuple_lagi(){
+    let mut data : (i32,f64,bool) = (10,7.7,true);
+    print!("{:?}", data);
+
+    let (a, b, c) = data; 
+    print!("{} {} {}",a, b, c);
+
+    data.0 = 20;
+    data.1 = 35.9;
+    data.2 = false;
+    print!("{:?}", data);
+}
+
+fn unit(){
+    println!("hello");
+}
+
+//tuple tapi gaada isinya
+#[test]
+fn tuple_kosong(){
+    let result: () = unit();
+    println!("{:?}", result);
+
+    let angga: () = ();
+    println!("{:?}", angga);
+}
+
+//array
+#[test]
+fn array(){
+    let array: [i32; 3] = [1,2,3];
+    
+    print!("{:?}", array);
+}
+
+//cara akses array
+#[test]
+fn array_akses(){
+    let array: [i32; 7] = [1,2,3,4,5,6,7];
+    
+    let a = array[0];
+    let b = array[1];
+    let c = array[2];
+    let d = array[3];
+    let e = array[4];
+    let f = array[5];
+    let g = array[6];
+
+    print!("{:?} {:?}", a, g);
+}
+
+//mutable array + ngitung jarak arraynya
+#[test]
+fn array_mut(){
+    let mut array: [i32; 7] = [1,2,3,4,5,6,7];
+    
+    let a = array[0];
+    let g = array[6];
+    print!("{} {}", a, g);
+
+    array[0] = 10;
+    array[6] = 20;
+    print!("{:?}", array);
+
+    let jarak: usize = array.len();
+    print!("{}", jarak);
+}
+
+//array 2 dimensi
+#[test]
+fn two_d_array(){
+    let two : [[i32;2];2]= [
+        [1,2],
+        [3,4]
+    ];
+    println!("{:?}", two);
+    println!("{}", two[0][0]);
+    println!("{}", two[0][1]);
+    println!("{}", two[1][0]);
+    println!("{}", two[1][1]);
+}
+
+//array 3 dimensi (dimensi selanjutnya tinggal di sesuain aja)
+#[test]
+fn arrayangga(){
+    let matrix : [[i32;3];3]= [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ];
+    println!("{:?}", matrix);
+    println!("{}", matrix[0][1]);
+    println!("{}", matrix[1][2]);
+    println!("{}", matrix[2][0]);
+}
+
+
+//constant 
+const MAXIMUM:i32 = 100;
+
+#[test]
+fn cons(){
+    const MINIMUM: i32 = 0;
+    println!("{} {}", MINIMUM, MAXIMUM);
+}
+
+
+//cara stak dan heap menejemen memory di rust
+#[test]
+fn stak_heap(){
+    function_a();
+    function_b();
+}
+
+fn function_a(){
+    let a = 10;
+    let b = String::from("purta");
+
+    println!("{}, {}", a, b);
+}
+
+fn function_b(){    
+    let a = 20;
+    let b = String::from("agus waluyo");
+
+    println!("{}, {}", a, b);
+}
